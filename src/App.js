d21a4db79c5b8f0cdc67fc6c4080d04710f48a0f
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2025-01-17 00:00:35 +0530

    Testing Tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my portfolio</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">Welcome to my portfolio</h1>
     </div>
   );
 }
