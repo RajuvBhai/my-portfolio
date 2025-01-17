6169b72e1e6dc62b7a5a3d4c38a1a04202fe6f11
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2025-01-17 18:28:39 +0530

    Creating Header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-500">Welcome to my portfolio</h1>
+      <Header/>
     </div>
   );
 }
