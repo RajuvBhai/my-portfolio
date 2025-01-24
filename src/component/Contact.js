e62789ac4937545fc97c88406c402d87e91d0355
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2025-01-25 01:01:10 +0530

    Directory Changes

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,5 +1,5 @@
 export default function Contact() {
-    return  <section className='flex flex-col text-white bg-primary px-5 py-32'>
+    return  <section id="contact" className='flex flex-col text-white bg-primary px-5 py-32'>
                 <div className='flex flex-col items-center'>
                     <h1 className='text-4xl border-secondary border-b-2 mb-5 w-[140px] font-bold'>Contact</h1>
                     <p className='pb-5'>If you want to discuss more in detail, please contact me</p>
