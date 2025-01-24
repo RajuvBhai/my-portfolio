e286b1ac7f66c4030fe6947286f065494f212aff
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2025-01-25 00:47:07 +0530

    Creating Footer section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './component/About';
+import Footer from './component/Footer';
 import Header from './component/Header';
 import Hero from './component/Hero';
 import Project from './component/Project';
@@ -15,6 +16,7 @@ function App() {
       <Project/>
       <Resume/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
