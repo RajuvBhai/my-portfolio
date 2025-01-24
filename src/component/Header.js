82967b44a1cbfe65867cecf841fbf56505b81426
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2025-01-25 01:20:06 +0530

    Corrections

--- src/component/Header.js
+++ src/component/Header.js
@@ -16,12 +16,12 @@ export default function Header() {
                     </ul>
                 </nav>
                 {toggleMenu && <nav className="block md:hidden">
-                    <ul className="flex flex-col text-white mobile-nav">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">Resume</a></li>
-                        <li><a href="#">Contact</a></li>
+                    <ul onClick={() => setToggleMenu(!toggleMenu)} className="flex flex-col text-white mobile-nav">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#project">Projects</a></li>
+                        <li><a href="#resume">Resume</a></li>
+                        <li><a href="#contact">Contact</a></li>
                     </ul>
                 </nav>}
                 <button onClick={() => setToggleMenu(!toggleMenu)} className='block md:hidden'><Bars3Icon className='text-white h-5'/></button>
